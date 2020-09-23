df0eb7e3812e811624d915a22e2e7222917647d0
orane <orane.orveillon@outlook.fr>
2020-09-23 14:24:37 +0200

    html et css connexion, début html ecritureArticle

---
+++ view/connexion.php
@@ -0,0 +1,22 @@
+<link rel="stylesheet" type="text/css" href="Style/connexion.css">
+
+<body>
+    <div class="grid-container">
+        <div class="cadre-connexion">
+
+            <label for="identifiant">Identifiant :</label><br>
+            <input type="text" id="identifiant"  size="30"><br>
+
+            <label for="mot de passe">Mot de passe :</label><br>
+            <input type="text" id="mot de passe"  size="30"><br>
+
+            <button type=""button">Valider</button>
+            <button type=""button">Annuler</button>
+
+        </div>
+    </div>
+</body>
+
+
+
+<?php
---
+++ view/ecritureArticle.php
@@ -0,0 +1,21 @@
+
+
+<body>
+<div class="grid-container">
+    <div class="cadre-connexion">
+
+        <label for="identifiant">Titre :</label><br>
+        <input type="text" id="titre"  size="30"><br>
+
+        <label for="mot de passe">Contenu du l'article :</label><br>
+        <input type="text" id="contenu de l'article"  width="60" minlength="100"><br>
+
+        <button type=""button">Valider</button>
+        <button type=""button">Annuler</button>
+
+    </div>
+</div>
+</body>
+
+
+
