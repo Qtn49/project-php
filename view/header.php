59f4c4727dec457e5b8422f6553436e51c2763ce
theog <bababoui@gmail.com>
2020-10-07 11:25:45 +0200

    Fix problem

--- view/header.php
+++ view/header.php
@@ -1,6 +1,15 @@
 <head>
     <div id="menu">
         <ul>
-           <li><a href="index.php"><b><font face ="Calibri" size ="6" >Bababoui</b></a></li<ul>
+            <li>
+                <a href="index.php"><font face="Calibri" size="6"><b>B</b>ababoui</li>
+            <div id="buton">
+                <a href="" class="btn btn-light float-right align-middle"><b>inscription</b></a>
+                <a href="" class="btn btn-outline-light float-right align-middle"><b>connexion</b></a>
+            </div>
+
+        </ul>
+
+
     </div>
 </head>
